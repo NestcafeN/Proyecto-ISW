92a30bbd9a586d31f334c62ee7d86868ae2d9835
alonsoalejara <alonsoignacioalejara@gmail.com>
2023-10-12 21:36:20 -0300

    Corrección controller de rúbrica

--- src/controllers/rubrica.controller.js
+++ src/controllers/rubrica.controller.js
@@ -20,3 +20,54 @@ export async function getRubrica(req, res) {
         res.status(500).json({ message: "Error al obtener la Rúbrica" });
     }
 }
+
+export async function createRubrica(req, res) {
+    try {
+        const rubrica = new Rubrica({
+            nombre: req.body.nombre,
+            tipoFondo: req.body.tipoFondo,
+            criterios: req.body.criterios,
+            puntajeMinimoAprobacion: req.body.puntajeMinimoAprobacion,
+            puntajeMaximoAprobacion: req.body.puntajeMaximoAprobacion,
+        });
+        const nuevaRubrica = await rubrica.save();
+        res.status(201).json(nuevaRubrica);
+    } catch (error) {
+        res.status(400).json({ message: "Error al crear Rúbrica" });
+    }
+}
+
+export async function updateRubrica(req, res) {
+    try {
+        const rubrica = await Rubrica.findById(req.params.id);
+        if (!rubrica) {
+            return res.status(404).json({ message: "Rúbrica no encontrada" });
+        }
+
+        rubrica.nombre = req.body.nombre;
+        rubrica.tipoFondo = req.body.tipoFondo;
+        rubrica.criterios = req.body.criterios;
+        rubrica.puntajeMinimoAprobacion = req.body.puntajeMinimoAprobacion;
+        rubrica.puntajeMaximoAprobacion = req.body.puntajeMaximoAprobacion;
+
+        const rubricaActualizada = await rubrica.save();
+        res.status(200).json(rubricaActualizada);
+    } catch (error) {
+        res.status(400).json({ message: "Error al actualizar Rúbrica" });
+    }
+}
+
+export async function deleteRubrica(req, res) {
+    try {
+        const rubrica = await Rubrica.findById(req.params.id);
+
+        if (!rubrica) {
+            return res.status(404).json({ message: "Rúbrica no encontrada" });
+        }
+
+        await rubrica.remove();
+        res.status(200).json({ message: "Rúbrica eliminada correctamente" });
+    } catch (error) {
+        res.status(400).json({ message: "Error al eliminar Rúbrica" });
+    }
+}
